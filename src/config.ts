8a2d9a3c2304eaac35724f0db20c4758acbdd7d2
koblakai <155496529+koblakai@users.noreply.github.com>
2024-10-19 19:06:45 -0400

    Update src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -6,4 +6,5 @@ export const config = {
   socialMedia: [],
   enableBlog: true,
   enableShop: true,
+  landingPageImage: "https://images.unsplash.com/photo-1616699002805-0741e1e4a9c5?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
 };
