eeb1cc281e6fc817048ea954aa2398d617ee1949
Simple Studio <noreply@simplestudio.com>
2024-10-19 17:04:30 -0400

    Add/update src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,1 @@
-
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import PublicLayout from './components/public/PublicLayout';
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/*" element={<PublicLayout />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
-  
+undefined
