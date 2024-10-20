f9ecf55e7ba1c9bc7b9f6db12419bc89078bcc4a
koblakai <155496529+koblakai@users.noreply.github.com>
2024-10-20 15:55:37 -0400

    Update src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const config = {
   socialMedia: [],
   enableBlog: true,
   enableShop: true,
-  landingPageImage: "https://images.unsplash.com/photo-1599901860904-17e6ed7083a0?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+  landingPageImage: "https://images.unsplash.com/photo-1603988363607-e1e4a66962c6?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
   classes: [{"name":"Vinyasa Flow","instructor":"Tiffany Blackwell","capacity":20,"time":"05:00","id":1729383204690,"description":"Slow flow vinyasa suitable for all!","locationId":"","date":"2024-10-21","room":"Yoga Studio 1"}],
   hasMultipleLocations: false,
   currentLocation: undefined,
