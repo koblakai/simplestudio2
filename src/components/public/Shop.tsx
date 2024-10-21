33fd3392e278960f14dd071d456ed64cdaca683f
koblakai <155496529+koblakai@users.noreply.github.com>
2024-10-20 21:34:56 -0400

    Update src/components/public/Shop.tsx

--- src/components/public/Shop.tsx
+++ src/components/public/Shop.tsx
@@ -1,18 +1,1 @@
-
-import React from 'react';
-import { config } from '../../config';
-
-const Shop: React.FC = () => {
-  if (!config.enableShop) {
-    return null;
-  }
-
-  return (
-    <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-      <h2 className="text-3xl font-extrabold text-gray-900 mb-8">Shop</h2>
-      {/* Implement shop items here */}
-    </div>
-  );
-};
-
-export default Shop;
+undefined
