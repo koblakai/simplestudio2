32f6d83c786eae4c1666961da12914dcb7d43e0b
koblakai <155496529+koblakai@users.noreply.github.com>
2024-10-20 21:34:56 -0400

    Update src/components/public/Blog.tsx

--- src/components/public/Blog.tsx
+++ src/components/public/Blog.tsx
@@ -1,18 +1,1 @@
-
-import React from 'react';
-import { config } from '../../config';
-
-const Blog: React.FC = () => {
-  if (!config.enableBlog) {
-    return null;
-  }
-
-  return (
-    <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-      <h2 className="text-3xl font-extrabold text-gray-900 mb-8">Latest Blog Posts</h2>
-      {/* Implement blog posts here */}
-    </div>
-  );
-};
-
-export default Blog;
+undefined
