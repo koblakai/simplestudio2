a9eecccd2e174a024c4bf81eb0a1aea5a9679e00
koblakai <155496529+koblakai@users.noreply.github.com>
2024-10-20 21:13:20 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        'firebase/app',
+        'firebase/auth',
+        'firebase/firestore',
+        'firebase/storage',
+        'firebase/functions'
+      ]
+    }
+  },
+  optimizeDeps: {
+    include: ['firebase/app', 'firebase/auth', 'firebase/firestore', 'firebase/storage', 'firebase/functions']
+  }
 });
